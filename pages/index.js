397822723296abc50a67c92de15a0202746237a9
madani93 <madani@citynet.ir>
2023-09-28 02:34:24 +0330

    display product

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,15 @@
  import Layout from "@/components/Layout";
+ import Product from "@/components/Product";
+ import productItems from "../data/products.json"
  function Home() {
   return (
-    <Layout title='Home Page'>Home page</Layout>
+    <Layout title='Home Page'>
+      <div className="grid grid-cols-1 gap-12 md:grid-cols-3 lg:grid-cols-4 ">
+        {productItems.map((pItem) => (
+          <Product item={pItem} key={pItem.slug}></Product>
+        ))}
+      </div>
+    </Layout>
   )
 }
 
